e161a0fb07f3d2fff0b9230a9d7a1564fbaa1959
yangmbin <1057207798@qq.com>
2018-01-31 14:46:03 +0800

    配置修改

--- runserver.py
+++ runserver.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     # app.run(debug=True, ssl_context='adhoc')
     #app.run(debug=True, host='0.0.0.0', port=80, threaded=True)
     #app.run(debug=True, host='0.0.0.0', port=80, threaded=True, ssl_context=('ssl/214295754730911.crt', 'ssl/214295754730911.key'))
-    # app.run(debug=True, threaded=True)
-    app.run(debug=True, threaded=True, host='192.168.1.86')
+    app.run(debug=True, threaded=True)
+    # app.run(debug=True, threaded=True, host='192.168.1.86')
